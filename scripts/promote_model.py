154ac5ebd5a07fd1aac833037f7d5893b9299a7b
aniketjayant953 <aniketjayant953@gmail.com>
2025-05-03 16:19:50 +0530

    add promote model script

---
+++ scripts/promote_model.py
@@ -0,0 +1,33 @@
+import os
+import mlflow
+
+def promote_model():
+    # Set up AWS MLflow tracking URI
+    mlflow.set_tracking_uri("http://ec2-13-61-19-60.eu-north-1.compute.amazonaws.com:5000/")
+
+    client = mlflow.MlflowClient()
+
+    model_name = "my_model"
+
+    # Archive the current production model
+    prod_versions = client.get_latest_versions(model_name, stages=["Production"])
+    for version in prod_versions:
+        client.transitions_model_version_stages(
+            name=model_name,
+            version=version.version,
+            stage="Archived"
+        )
+
+    # Get the latest version in staging
+    latest_version_staging = client.get_latest_vesion(model_name, stages=["Staging"])[0].version
+
+    # Promote the new model to production
+    client.transition_model_version_stage(
+            name=model_name,
+            version=latest_version_staging,
+            stage="Production"
+    )
+    print(f"Model version {latest_version_staging} promotes to Production")
+
+if __name__ == "__main__":
+    promote_model()
