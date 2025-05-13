bd45a2e5deb94c6b8ae831a33b72b71831273c8f
aniketjayant953 <aniketjayant953@gmail.com>
2025-05-13 17:48:18 +0530

    add appspec.yml

--- deploy/scripts/install_dependencies.sh
+++ deploy/scripts/install_dependencies.sh
@@ -20,3 +20,9 @@ sudo apt-get install -y unzip curl
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip"
 unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu
 sudo /home/ubuntu/aws/install
+
+# Add 'ubuntu' user to the 'docker' group to run Docker commands without 'sudo'
+sudo usermod -aG docker ubuntu
+
+# Clean up the AWS CLI installation files
+rm -rf /home/ubuntu/awscliv2.zip /home/ubuntu/aws
