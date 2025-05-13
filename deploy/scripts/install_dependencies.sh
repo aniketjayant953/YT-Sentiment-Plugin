341f462b04b0fef531d88ef3f1338efc8a6121e8
aniketjayant953 <aniketjayant953@gmail.com>
2025-05-13 14:21:44 +0530

    add appspec.yml

---
+++ deploy/scripts/install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure that the script runs in non-interactive mode
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package lists
+sudo apt-get update -y
+
+# Install Docker
+sudo apt-get install -y docker.io
+
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Install necessary utilities
+sudo apt-get install -y unzip curl
+
+# Download and install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip"
+unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu
+sudo /home/ubuntu/aws/install
